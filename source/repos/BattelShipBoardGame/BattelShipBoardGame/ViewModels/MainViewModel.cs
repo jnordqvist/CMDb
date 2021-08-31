3f108deb88f88a1be69a48bbb8fe50bc6efd4360
jnordqvist <73642406+jnordqvist@users.noreply.github.com>
2021-08-31 12:31:38 +0200

    mvvm 1 done

---
+++ source/repos/BattelShipBoardGame/BattelShipBoardGame/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using BattleShipBoardGame.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace BattleShipBoardGame.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        //public string WizardName { get; set; } = "Harry Potter";
+        public ICommand CreateWizardCommand { get; }
+        private string wizardName;
+
+        public string WizardName
+        {
+            get { return wizardName; }
+            set 
+            { 
+                wizardName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            CreateWizardCommand = new CreateWizardCommand(this);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
