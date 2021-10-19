b997910becc2580223f310ae1c1062bd6f9b4886
erikwesterberg1 <51288842+erikwesterberg1@users.noreply.github.com>
2021-10-19 14:08:14 +0200

    progress

--- interaktivWebb/interaktivWebb/Controllers/HomeController.cs
+++ interaktivWebb/interaktivWebb/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace interaktivWebb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Movies()
         {
             return View();
         }
